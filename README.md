6bfbfe878ef6e3bc4c0caeacca7a15bbef43f681
FLY_U <zhanghao97hao@163.com>
2018-06-08 14:09:08 +0800

    新增登录界面，还没有实现其中的方法

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AndroidNew
 # 该安卓项目用于学习安卓，开发期间要整理详细备注和文档说明
+
+# 登录界面涉及到的知识点
+
+# 公共方法
+> 去掉标题栏
+>>if (getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
--- app/src/main/java/com/example/androidnew/MainActivity.java
+++ app/src/main/java/com/example/androidnew/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.androidnew;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.phonenumber)
+    EditText phonenumber;
+    @BindView(R.id.del_phonenumber)
+    ImageView delPhonenumber;
+    @BindView(R.id.password)
+    EditText password;
+    @BindView(R.id.del_password)
+    ImageView delPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        //-----------------去掉标题栏--------------------------//
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        //-----------------去掉标题栏--------------------------//
     }
 }
